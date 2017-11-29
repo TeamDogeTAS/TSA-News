f0c439f98590f995c86d04c8cc257bb4a83bb8cd
jaeshin <jae.young.shin@accenturefederal.com>
2017-11-29 15:50:31 -0500

    Removed unneccessary steps and combined into one class

--- src/test/java/gov/dhs/tsa/runners/Test.java
+++ src/test/java/gov/dhs/tsa/runners/Test.java
@@ -5,13 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Test {
-    public static void main(String[] args) {
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.tutorialspoint.com/javascript/javascript_dialog_boxes.htm");
-        WebElement frame = driver.findElement(By.xpath("/html/body/div[3]/div[1]/div/div[2]/div[1]/div/iframe[2]"));
-        driver.switchTo().frame(frame);
-        driver.findElement(By.cssSelector("body > form > input[type=\"button\"]")).click();
-        driver.switchTo().alert().dismiss();
-    }
-}
+//public class Test {
+//    public static void main(String[] args) {
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://www.tutorialspoint.com/javascript/javascript_dialog_boxes.htm");
+//        WebElement frame = driver.findElement(By.xpath("/html/body/div[3]/div[1]/div/div[2]/div[1]/div/iframe[2]"));
+//        driver.switchTo().frame(frame);
+//        driver.findElement(By.cssSelector("body > form > input[type=\"button\"]")).click();
+//        driver.switchTo().alert().dismiss();
+//    }
+//}
